cbe1af0298e5d922601536e7d95b48bd706f289e
Slocx <33150302+Slocx@users.noreply.github.com>
2021-11-12 15:45:24 +0100

    commit get Films

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmDetailComponent } from './film/film-detail/film-detail.component';
+import { FilmsPageComponent } from './film/films-page/films-page.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // redirection quand l'utilisateur arrive sur le chemin /
+  { path: "", redirectTo: "films", pathMatch: "full" },
+  {
+    path: "films", children: [
+      { path: "", component: FilmsPageComponent, pathMatch: "full" },
+      { path: ":id", component: FilmDetailComponent, pathMatch: "full" },
+    ]
+  },
+    // Wildcard pour afficher un composant en cas de non existance de la page
+    { path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { FilmsPageComponent } from './film/films-page/films-page.component';
+import { FilmDetailComponent } from './film/film-detail/film-detail.component';
+import { FilmComponent } from './film/films-page/film/film.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    FilmsPageComponent,
+    FilmDetailComponent,
+    FilmComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
